7900e70643e3988512d6e057913a1c0238a5f324
Igor Popov <igor.v.popov@outlook.com>
2023-04-16 23:32:47 +0600

    add setup for train

--- s3/init.sh
+++ s3/init.sh
@@ -1,5 +1,6 @@
 mc config host add myminio http://s3:$MINIO_PORT $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 mc mb myminio/$MINIO_BUCKET
 mc policy set public myminio/$MINIO_BUCKET
-mc cp /opt/sessions.csv myminio/$MINIO_BUCKET
-mc cp /opt/model.cbm myminio/$MINIO_BUCKET/$MINIO_FOLDER/$MINIO_WEIGHTS
+mc cp /opt/$MINIO_SESSIONS_CSV myminio/$MINIO_BUCKET/$MINIO_DATA_FOLDER/$MINIO_SESSIONS_CSV
+mc cp /opt/$MINIO_WEIGHTS myminio/$MINIO_BUCKET/$MINIO_FOLDER/$MINIO_WEIGHTS
+mc cp /opt/$MINIO_VENUES_CSV myminio/$MINIO_BUCKET/$MINIO_DATA_FOLDER/$MINIO_VENUES_CSV
